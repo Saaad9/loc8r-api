1de49a003d205b9e1fc2f30af1839123bd655750
saaad99 <110652278+Saaad9@users.noreply.github.com>
2023-12-05 17:34:32 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-require('dotenv').config()
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -7,31 +6,16 @@ var logger = require('morgan');
 const passport = require('passport');
 require('./app_api/models/db');
 require('./app_api/config/passport');
-const cors = require('cors');
+require('dotenv').config()
 
+const indexRouter = require('./app_server/routes/index');
 const apiRouter = require('./app_api/routes/index');
-
-// const indexRouter = require('./app_server/routes/index');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname,'app_server', 'views'));
+app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'app_public', 'build')));
-app.use(express.static(path.join(__dirname, 'app_public')));
-app.use(passport.initialize());
-
-app.use(cors());
-
-// app.use('/', indexRouter);
-app.use('/api', apiRouter);
 
 app.use('/api', (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -40,38 +24,43 @@ app.use('/api', (req, res, next) => {
   next();
 })
 
-app.use('/api', (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, \
-  Content-Type, Accept, Authorization');
-  next();
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'app_public', 'build')));
+app.use(passport.initialize());
+
+app.use((err, req, res, next) => {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).json({ "message": err.name + ": " + err.message });
+  }
 })
 
-// app.get('*', function(req, res, next) {
-//   res.sendFile(path.join(__dirname, 'app_public', 'build','index.html'));
-// });
 
-app.get(/(\/about)|(\/location\/[a-z0-9]{24})/,function(req,res,next) {
-  res.sendFile(path.join(__dirname, 'app_public', 'build','index.html'));
+app.use('/', indexRouter);
+app.use('/api', apiRouter);
+
+
+app.get('*', function (req, res, next) {
+  res.sendFile(path.join(__dirname, 'app_public', 'build', 'index.html'));
+})
+
+app.get(/(\/about)|(\/location\/[a-z0-9]{24})/, function (req, res, next) {
+  res.sendFile(path.join(__dirname, 'app_public', 'build', 'index.htmI'));
 });
 
-//error handler
-//Catch unauthorised errors
-app.use((err,req,next) => {
-  if(err.name === 'UnauthorizedError') {
-    res
-      .status(401)
-      .json({"message" : err.name + ": "+ err.message });
-  }
-});
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
